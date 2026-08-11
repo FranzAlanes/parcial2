6178d0dc94d814715c6df0af9feccdcf659b0b5e
franz alanes fuentes <franz.alanes@uab.edu.bo>
2026-08-11 19:40:28 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const formulario = document.querySelector("#form-helados");
-const aviso = document.querySelector("#aviso-helados");
-function revisarPedido(event) {
-}
-formulario.addEventListener("submit", revisarPedido);
